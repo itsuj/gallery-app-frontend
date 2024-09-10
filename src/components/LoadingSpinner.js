cbc9c3bc616f1cf6c67b7568c779db60f83aac93
sujitj50 <sujitkumarrjha@gmail.com>
2024-09-10 17:03:39 +0530

    changing the spinner text

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -5,7 +5,7 @@ const LoadingSpinner = () => {
     return (
         <div className="spinner-container">
         <div className="loading-spinner"></div>
-        Loading, please wait...
+        Due to server inactivity, the gallery may take up to 50 seconds or more to load initially. ...Please wait for the images to appear
         </div>
     )
 }
